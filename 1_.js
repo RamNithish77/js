efd951fde28d7d09539d8bf77b6aa86b4a21871e
Sriram <136118844+RamNithish77@users.noreply.github.com>
2024-01-05 12:42:15 +0530

    Add files via upload

---
+++ 1_.js
@@ -0,0 +1,17 @@
+console.log("hi")
+
+let a=23;
+let b=45;
+let c="harry";
+let d=true;
+
+console.log(a,b,c,d);
+console.log(typeof a,typeof b,typeof c,typeof d);
+
+let o = {
+    "name":"sriram",
+    "my height" : 12
+}
+ console.log(o);
+ o.name="harry";
+ console.log(o)
---
+++ if_else.js
@@ -0,0 +1,15 @@
+console.log("these are if else statements");
+
+let a=12;
+
+if(a>18){
+    console.log("i am eligibe to vote")
+}
+
+else{
+    console.log("we are not eligible to vote")
+}
+
+//operators + - * / ** ++ -- %
+//assignment operators = += -= *= /=
+//comparision operators == >= <= !=
